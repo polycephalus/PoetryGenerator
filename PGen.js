63a7be5b5aae69232c13e8f83074e805e0176aca
polycephalus <juamalin@gmail.com>
2019-05-14 15:58:25 +0200

    added checks for syll capacity, metre fit in verses

--- PGen.js
+++ PGen.js
@@ -20,11 +20,13 @@ class Stanza {
         var found = false;
         while(!found) {
             this.sentences[0] = grammar.flatten('#ES#');
-            console.log(this.sentences[0]);
             if (this.sentenceSplit(0)) {
+                //limit check
                 console.log('VALID. ');
-                found = true; //-----------------------------------------temp!!
+                found = this.syllLimitCheck(0);
+                // found = true; //-----------------------------------------temp!!
             } else {
+                //continue
                 console.log('INVALID. ');
             }
         }
@@ -35,12 +37,20 @@ class Stanza {
             for (var j = 0; j < 2; j++) {
                 this.verses[i][j] = {
                     verseStr: '',
+                    metre: 0,
+                    words: function() {
+                        return this.verseStr.match(/\w+/g);
+                    },
                     blanks: function() {
                         return this.verseStr.match(/_\w+/g);
                     }
                 }
             }
         }
+        this.verses[0][0].metre = 8;
+        this.verses[0][1].metre = 8;
+        this.verses[1][0].metre = 8;
+        this.verses[1][0].metre = 6;
     }
 
     sentenceSplit(senID) {
@@ -58,13 +68,13 @@ class Stanza {
             //no possible split
             return false;
         } else if (min_syll.length == 2) {
-            this.verses[senID][0] = poss_splits[0];
-            this.verses[senID][1] = poss_splits[1];
+            this.verses[senID][0].verseStr = poss_splits[0];
+            this.verses[senID][1].verseStr = poss_splits[1];
             return true;
         } else {
             var splitID = this.getBalancedSplit(min_syll);
-            this.verses[senID][0] = poss_splits.slice(0, splitID).join(' ');
-            this.verses[senID][1] = poss_splits.slice(splitID, poss_splits.length).join(' ');
+            this.verses[senID][0].verseStr = poss_splits.slice(0, splitID).join(' ');
+            this.verses[senID][1].verseStr = poss_splits.slice(splitID, poss_splits.length).join(' ');
             return true;
         }
     }
@@ -72,18 +82,19 @@ class Stanza {
     getMinSyll(verse) {
         //takes verse str, returns num of minSyll
         var wordList = verse.match(/\w+/g); //arr of words
-        var minTwo = RegExp(/_JJ|_Vpast/g); //words that consist of at least 2 syllables
+        // var minTwo = RegExp(/_JJ|_Vpast/g); //words that consist of at least 2 syllables
 
         var minSyllCount = 0;
 
         wordList.forEach(word => {
+            var minTwo = RegExp(/_JJ|_Vpast/g);
             if(minTwo.test(word)) {
                 minSyllCount += 2;
             } else {
                 minSyllCount++;
             }
         });
-
+        
         return minSyllCount;
     }
 
@@ -108,11 +119,33 @@ class Stanza {
 
         return balanced_splitID;
     }
+
+    syllLimitCheck(senID) {
+        //checking syllable capacity
+        //if syll to be filled > capacity return false
+        for (let i = 0; i < this.verses[senID].length; i++) {
+            var verse = this.verses[senID][i];
+
+            var capacity = verse.blanks().length * 3; //max 3 syllables/word
+            var filledLen = verse.words().length - verse.blanks().length; //all words - blanks
+            var toDistLen = verse.metre - filledLen;
+
+            var minSyll = this.getMinSyll(verse.verseStr); //min amount of syllable it takes to fill the blanks
+
+            //check syllable capacity of verse
+            //check if verse fits into metre
+            if(toDistLen>capacity || minSyll>verse.metre) return false;
+        }
+        return true;
+    }
 }
 
 var s = new Stanza();
 s.initStanza();
-// console.log(s.verses);
+
+for (var verse in s.verses[0]) {
+    console.log(s.verses[0][verse].verseStr);
+}
 
 //0     00
 //1     01
